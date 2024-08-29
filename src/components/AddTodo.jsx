061bf683c98fefa04eb6e51cdf20397c495713da
Ayush singh <ayush0346.be21@chitkara.edu.in>
2024-08-29 15:13:31 +0530

    first commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,68 @@
+import { useState , useRef} from "react";
+import { useContext } from "react";
+import { TodoItemsContext } from "../store/todo-items-store";
+
+function AddTodo() {
+  const { addNewItem } = useContext(TodoItemsContext);
+
+  // const [todoName, setTodoName] = useState();
+  // const [dueDate, setDueDate] = useState();
+
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
+
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  // };
+
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault(); 
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+    addNewItem(todoName, dueDate);
+    // setDueDate("");
+    // setTodoName("");
+  };
+
+  return (
+    <div className="container text-center">
+      <form className="row kg-row" onSubmit={handleAddButtonClicked}>
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            ref={todoNameElement}
+            // value={todoName}
+            // onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input
+            type="date"
+            ref={dueDateElement}
+            // value={dueDate}
+            // onChange={handleDateChange}
+          />
+        </div>
+        <div className="col-2">
+          <button
+            // type="button"
+            type="submit"
+            className="btn btn-success kg-button"
+            // onClick={handleAddButtonClicked}
+          >
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodo;
